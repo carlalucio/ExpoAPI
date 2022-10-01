626f51c651ccc934d552198e63be60666e95ce32
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-10-01 10:55:31 -0300

    criacao da rota BuscarPorId, Atualizar e Deletar

--- Controllers/ProjetoController.cs
+++ Controllers/ProjetoController.cs
@@ -1,4 +1,5 @@
-﻿using ExoAPI.Repositories;
+﻿using ExoAPI.Models;
+using ExoAPI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +27,73 @@ namespace ExoAPI.Controllers
             catch (Exception e)
             {
                 throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Projeto projeto = _projetoRepository.BuscarPorId(id);
+                if(projeto == null)
+                    return NotFound();
+                return Ok(projeto);
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            try
+            {
+                _projetoRepository.Cadastrar(projeto);
+                return StatusCode(204);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            try
+            {
+                Projeto projetoBuscado = _projetoRepository.BuscarPorId(id);
+                if (projetoBuscado == null)
+                    return NotFound();
+
+                _projetoRepository.Atualizar(id, projeto);
+                return StatusCode(204);
+
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                Projeto projeto = _projetoRepository.BuscarPorId(id);
+                if (projeto == null)
+                    return NotFound();
+
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
