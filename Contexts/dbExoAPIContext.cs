67a5013b20099ef5acb8bd4ec487d83a7251b1f6
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-10-01 08:19:47 -0300

    first commit

---
+++ Contexts/dbExoAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using ExoAPI.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+namespace ExoAPI.Contexts
+{
+    public class dbExoAPIContext : DbContext
+    {
+        public dbExoAPIContext() { }
+
+        public dbExoAPIContext(DbContextOptions<dbExoAPIContext> options) : base(options) 
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=CL\\SQLEXPRESS; initial catalog= dbExoAPI; Integrated Security=true");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+
+    }
+}
